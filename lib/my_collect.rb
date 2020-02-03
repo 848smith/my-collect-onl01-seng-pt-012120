9c28a4928612dcacfc318dbc5e6b3954c68b3a86
Andrew Smith <848smith@gmail.com>
2020-02-03 06:49:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,9 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-    new_array << yield(array[i].upcase)
+    new_array << yield(array[i])
     i += 1
   end
   new_array
 end
 
-my_collect(array) {|i| i.split(" ").first}
